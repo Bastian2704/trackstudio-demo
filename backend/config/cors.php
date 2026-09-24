ceee67eee332aac12cbfbc3d62390426fad33c27
Bastian2704 <sebastianabad2704@gmail.com>
2026-09-24 11:08:46 -0500

    fix(backend): TS-40 run pint

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,9 +20,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => array_values(array_filter(array_map(
-    'trim',
-    explode(',', (string) env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173'))
-))),
+        'trim',
+        explode(',', (string) env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173'))
+    ))),
 
     'allowed_origins_patterns' => [],
 
